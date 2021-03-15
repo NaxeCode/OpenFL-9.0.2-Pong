0a79eec4cd90c7d264db35f6ccb9118f4b924770
Aladdin Lee <Lucas_Naruto_AE@Yahoo.com>
2021-03-14 22:15:48 -0700

    Set paddles' and ball position

--- Source/Main.hx
+++ Source/Main.hx
@@ -6,6 +6,11 @@ import openfl.display.Sprite;
 import openfl.Assets;
 
 class Main extends Sprite {
+	var paddle1:Platform;
+	var paddle2:Platform;
+
+	var ball:Ball;
+
 	public function new() {
 		super();
 
@@ -15,10 +20,19 @@ class Main extends Sprite {
 		bitmap.x = (stage.stageWidth - bitmap.width) / 2;
 		bitmap.y = (stage.stageHeight - bitmap.height) / 2;
 
-		var platform = new Platform();
-		addChild(platform);
+		var paddle1 = new Platform();
+		paddle1.x = 5;
+		paddle1.y = 200;
+		addChild(paddle1);
 
-		var ball = new Ball();
+		var paddle2 = new Platform();
+		paddle2.x = 400;
+		paddle2.y = 200;
+		addChild(paddle2);
+
+		ball = new Ball();
+		ball.x = 250;
+		ball.y = 250;
 		addChild(ball);
 	}
 }
